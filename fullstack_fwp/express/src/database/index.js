d1b0b0507f91bfa758706ca7afcf4fca8937d74d
s3897250 <s3897250@student.rmit.edu.au>
2022-09-26 16:41:04 +1000

    src

--- fullstack_fwp/express/src/database/index.js
+++ fullstack_fwp/express/src/database/index.js
@@ -1,6 +1,47 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const config = require("./config.js");
 
+const db = {
+  Op: Sequelize.Op
+};
 
+// Create Sequelize.
+db.sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.DIALECT
+});
+
+// Include models.
+db.user = require("./models/user.js")(db.sequelize, DataTypes);
+
+
+// Learn more about associations here: https://sequelize.org/master/manual/assocs.html
+
+// Include a sync option with seed data logic included.
+db.sync = async () => {
+  // Sync schema.
+  await db.sequelize.sync();
+
+  // Can sync with force if the schema has become out of date - note that syncing with force is a destructive operation.
+  // await db.sequelize.sync({ force: true });
+  
+  await seedData();
+};
+
+async function seedData() {
+  const count = await db.user.count();
+
+  // Only seed data if necessary.
+  if(count > 0)
+    return;
+
+  const argon2 = require("argon2");
+
+  let hash = await argon2.hash("abc123", { type: argon2.argon2id });
+  await db.user.create({ username: "ng", password_hash: hash, first_name: "Nate", last_name : "g" });
+
+  hash = await argon2.hash("def456", { type: argon2.argon2id });
+  await db.user.create({ username: "pk", password_hash: hash, first_name: "Parth", last_name : "k" });
+}
 
 module.exports = db;
